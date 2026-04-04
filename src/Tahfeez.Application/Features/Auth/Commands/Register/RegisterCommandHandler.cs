a594f1dc9fb5a6a30286045e021f39936123412a
AhmedShaban <ahmed.sh5121999@gmail.com>
2026-04-04 22:18:50 +0200

    Implement RegisterCommandHandler skeleton and validator stub
    Refactored RegisterCommandHandler to implement IRequestHandler,
    added dependency injection for UserManager, and introduced a
    Handle method with initial logic. Added a stub for
    RegisterCommandValidator and cleaned up using directives.
    Closed class definition in LoginCommandHandler.cs.

--- src/Tahfeez.Application/Features/Auth/Commands/Register/RegisterCommandHandler.cs
+++ src/Tahfeez.Application/Features/Auth/Commands/Register/RegisterCommandHandler.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
-
+using Tahfeez.Domain.Entities.Users;
+using Tahfeez.SharedKernal.Common;
 namespace Tahfeez.Application.Features.Auth.Commands.Register
 {
-    public class RegisterCommandHandler
+    public class RegisterCommandHandler: IRequestHandler<RegisterCommand, Result>
     {
+        private readonly UserManager<Domain.Entities.Users.User> _userManager;
+        public RegisterCommandHandler(UserManager<Domain.Entities.Users.User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<Result> Handle(RegisterCommand request, CancellationToken cancellationToken = default)
+        {
+            await _userManager.FindByEmailAsync(request.Email);
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ src/Tahfeez.Application/Features/Auth/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tahfeez.Application.Features.Auth.Commands.Register
+{
+    // here using FluentValidation to validate the RegisterCommand
+    public class RegisterCommandValidator
+    {
+    }
+}
